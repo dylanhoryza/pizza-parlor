209d938385036516ec4437b5ce48db4e166891bd
dylan horyza <horyza.dylan@gmail.com>
2024-02-07 14:53:14 -0700

    added routes for handling pizzas

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const toppingsRoutes = require('./toppingsRoutes');
+const pizzaRoutes = require('./pizzaRoutes');
 
 router.use('/toppings', toppingsRoutes);
+router.use('/pizzas', pizzaRoutes)
 
 
 module.exports = router;
